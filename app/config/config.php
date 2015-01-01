555e1156b61bf479eddea9b2421590b40da71201
simonhochrein <simon@ministrycrm.com>
2014-12-31 18:04:16 -0600

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@ return new \Phalcon\Config(array(
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
-        'password'    => '1111',
+        'password'    => '',
         'dbname'      => 'auth',
     ),
     'application' => array(
